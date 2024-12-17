0bc3ca70491902078823fe556d18d26230b05f15
Philipp Gülink <philipp.guelink@icloud.com>
2024-12-17 12:13:49 +0100

    Standarddomain aus Variable entfernt

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "hosted_zone" {
   description = "Hosted Zone, in der die Subdomain angelegt werden soll. Achtung muss zwingend in Route53 verwaltet sein!"
   type        = string
-  default     = "guelink.services"
+  default     = "yourdomainatroute53.example"
 }
 
 variable "subdomain" {
